d2283139d9da14e816f9e5d710364cb5359497dd
John Cardenas <Cardenas.John.J@gmail.com>
2019-04-05 16:00:57 -0700

    Add attribution

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # emu2mqtt
 Export Rainforest Automation EMU-2 energy monitoring data to MQTT
+
+## Attribution
+- This script is derived from the excellent [emu2influx](https://github.com/abaker/emu2influx) project by Alex Baker. Credit for the basic flow of the script and EMU API interaction goes to him.
+- This script uses the [Emu-Serial-API](https://github.com/rainforestautomation/Emu-Serial-API) by Rainforest Automation.
--- emu2mqtt.py
+++ emu2mqtt.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python2
+
+## emu2mqtt
+# Export Rainforest Automation EMU-2 energy monitoring data to MQTT
+
+## Attribution
+# This script is derived from the excellent [emu2influx](https://github.com/abaker/emu2influx) project by Alex Baker. Credit for the basic flow of the script and EMU API interaction goes to him.
+# This script uses the [Emu-Serial-API](https://github.com/rainforestautomation/Emu-Serial-API) by Rainforest Automation.
+
 import logging
 import paho.mqtt.client as mqtt
 from datetime import datetime
